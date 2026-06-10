a97be8976fa68028b890f280b66f518c70410664
Ashley ⌨️ <ashuhleex03@outlook.com>
2026-06-09 23:30:35 -0400

    style: add custom colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+
+class ThemeColor {
+  static const MaterialColor fuchsia = MaterialColor(
+    0xFFFFBAE2, // base color
+    {
+      50:  Color(0xFFFFF5FC),
+      100: Color(0xFFEFE9ED),
+      200: Color(0xFFFFDDF1),
+      300: Color(0xFFFFD6EE),
+      400: Color(0xFFFFC8E8),
+      500: Color(0xFFFFBAE2),
+      600: Color(0xFFFFA1D8),
+      700: Color(0xFFFF96D3),
+      800: Color(0xFFF0A4D1),
+      900: Color(0xFFE464AE),
+    },
+  );
+  static const MaterialColor teal = MaterialColor(
+    0xFFBAF2FF, // base color
+    {
+      50:  Color(0xFFFDFFFF),
+      100: Color(0xFFF8FEFF),
+      200: Color(0xFFF4FDFF),
+      300: Color(0xFFEDFBFF),
+      400: Color(0xFFDEF8FF),
+      500: Color(0xFFBAF2FF),
+      600: Color(0xFF80C5D5),
+      700: Color(0xFF00D0FF),
+      800: Color(0xFF00ABD1),
+      900: Color(0xFF006075),
+    },
+  );
+}
