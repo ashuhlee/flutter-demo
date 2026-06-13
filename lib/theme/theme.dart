205e4b850ead8e7e6536cca12b69c6eb4e24e12e
Ashley ⌨️ <ashuhleex03@outlook.com>
2026-06-12 22:28:30 -0400

    style: add custom font

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -5,5 +5,6 @@ import 'package:todo_app/theme/colors.dart';
 class AppTheme {
   static final light = ThemeData(
     scaffoldBackgroundColor: ThemeColor.fuchsia.shade50,
+    fontFamily: 'Sofia',
   );
 }
